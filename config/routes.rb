358a01d78670fcba59d2694fbfadbdc6a4f503b6
PeyLun <peylun@gmail.com>
2018-01-18 02:14:58 -0500

    Added categories integration testing

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   
   resources :articles
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 
   # Example resource route with options:
   #   resources :products do
