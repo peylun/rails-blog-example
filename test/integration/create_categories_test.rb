c73a05782ca57b18f6ce4bef7a71e267df829eb6
PeyLun <peylun@gmail.com>
2018-01-18 02:50:46 -0500

    Added categories list integration testing

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-
   test "get new category form and create category" do
     get new_category_path
     assert_template 'categories/new'
