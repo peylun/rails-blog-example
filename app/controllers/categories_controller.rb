15d661aa2cde11426d2d094e9014fedb985d4059
PeyLun <peylun@gmail.com>
2018-01-18 03:35:54 -0500

    Added categories to nav

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+  
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 5)
+    @categories = Category.paginate(page: params[:page], per_page: 10)
   end
   
   def new
@@ -23,5 +25,11 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+    def require_admin
+      if !logged_in? || !current_user.admin?
+        flash[:danger] = "Only admin users can perform that action"
+        redirect_to categories_path
+      end
     end
 end
